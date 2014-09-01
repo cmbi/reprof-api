716a0ea0bbf4def087a8d88ea36f094309824b57
Coos Baakman <cbaakman@gmail.com>
2014-09-01 15:26:18 +0200

    Added warnings for when reprof installation is missing.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+which reprof || echo error, no reprof installed!
+
 export REPROF_REST_SETTINGS='../dev_settings.py'
 gunicorn --log-file=- -k gevent -b 127.0.0.1:5000 reprof_rest.application:app
