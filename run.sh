4630a48afbef495789f5050b525e86f0031066d2
Jon Black <Jon.Black@radboudumc.nl>
2014-10-28 16:36:37 +0100

    Don't check for reprof in shell script either

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-which reprof || echo error, no reprof installed!
-
 export REPROF_REST_SETTINGS='../dev_settings.py'
 gunicorn --log-file=- -k gevent -b 127.0.0.1:5000 reprof_rest.application:app
